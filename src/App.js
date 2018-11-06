158662222d5f31c63f9545c04eee40a3eb8464e9
amiloszewicz <miloszewicz.adam@gmail.com>
2018-11-06 15:37:41 +0100

    feat app basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>React here!</p>
-    </div>
-  );
-};
+import { runInThisContext } from "vm";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>To do list</h1>
+      </div>
+    )
+  }
+}
+
+class NewToDo extends React.Component {
+  render() {
+    return (
+      <div>
+        <input></input>
+        <button>Add</button>
+      </div>
+    )
+  }
+}
+
+class ToDoList extends React.Component {
+  render() {
+    return (
+      <ToDoItem />
+    )
+  }
+}
+
+class ToDoItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <checkbox/>
+        <span></span>
+        <button>del</button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <NewToDo />
+        <ToDoList />
+      </div>
+    )
+  }
+}
+
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
