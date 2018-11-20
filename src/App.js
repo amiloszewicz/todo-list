c77fe591022395f60e548199418641a30ae00351
amiloszewicz <miloszewicz.adam@gmail.com>
2018-11-20 13:34:29 +0100

    feat functions, ref and arguments for todolist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TodoList from './TodoList'
-import Header from './Header'
+import TodoList from './TodoList';
+import Header from './Header';
 
 import { runInThisContext } from 'vm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: { text: '', key: '' }
+    };
+    this.inputElement = React.createRef();
+  }
+  handleInput = e => {
+    console.log(`hello input`);
+  };
+  addItem = () => {
+    console.log(`hello add item`);
+  };
   render() {
     return (
       <div>
         <Header />
-        <ToDoList addItem={this.showInputValue} />
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem.text}
+        />
       </div>
     );
   }
