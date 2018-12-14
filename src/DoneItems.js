0ad02d812e63f46c74ba485f3df819c184786dd5
amiloszewicz <miloszewicz.adam@gmail.com>
2018-12-14 21:34:06 +0100

    refactor done items to be crossed out, no longer relevant

--- src/DoneItems.js
+++ src/DoneItems.js
@@ -3,7 +3,11 @@ import style from './main.css';
 
 class DoneItems extends Component {
   doneItem = item => {
-    return <li key={item.key}>{item.text}</li>;
+    return (
+      <li key={item.key} className='doneItem'>
+        <s>{item.text}</s>
+      </li>
+    );
   };
   render() {
     const doneEntries = this.props.doneEntries;
